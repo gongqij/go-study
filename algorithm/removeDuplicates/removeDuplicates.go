3a734bfa57b7dd1ed7d191353ccfea83f9db1764
gongqi <gongqi@ntmer.com>
2022-11-06 10:27:54 +0800

    update algorithm.removeDuplicates

--- algorithm/removeDuplicates/removeDuplicates.go
+++ algorithm/removeDuplicates/removeDuplicates.go
@@ -1,9 +1,42 @@
 package removeDuplicates
 
-func removeDuplicates(nums []int) []int {
+//升序数组去重
+func ascArrayRemoveDuplication(nums []int) []int {
 	slow := 1
 	for fast := 1; fast < len(nums); fast++ {
 		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return nums[:slow]
+}
+
+//无序数组去重1
+func arrayRemoveDuplication1(nums []int) []int {
+	temp := make(map[int]struct{})
+	for _, num := range nums {
+		temp[num] = struct{}{}
+	}
+	var res []int
+	for k := range temp {
+		res = append(res, k)
+	}
+	return res
+}
+
+//无序数组去重2
+func arrayRemoveDuplication2(nums []int) []int {
+	slow := 1
+	for fast := 1; fast < len(nums); fast++ {
+		flag := false
+		for j := fast - 1; j >= 0; j-- {
+			if nums[fast] == nums[j] {
+				flag = true
+				break
+			}
+		}
+		if !flag {
 			nums[slow] = nums[fast]
 			slow++
 		}
--- algorithm/removeDuplicates/removeDuplicates_test.go
+++ algorithm/removeDuplicates/removeDuplicates_test.go
@@ -2,9 +2,18 @@ package removeDuplicates
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"sort"
 )
 
 func Test_removeDuplicates(t *testing.T) {
-	res := removeDuplicates([]int{2, 4, 5, 5, 7, 7})
-	t.Logf("removeDuplicates:%v", res)
+	res1 := ascArrayRemoveDuplication([]int{2, 4, 5, 5, 7, 7})
+	assert.Equal(t, res1, []int{2, 4, 5, 7})
+	res2 := arrayRemoveDuplication1([]int{2, 4, 5, 5, 7, 7})
+	sort.Ints(res2)
+	assert.Equal(t, res2, []int{2, 4, 5, 7})
+	res3 := arrayRemoveDuplication2([]int{5, 2, 5, 2, 3, 4, 5, 1})
+	sort.Ints(res3)
+	assert.Equal(t, res3, []int{1, 2, 3, 4, 5})
 }
